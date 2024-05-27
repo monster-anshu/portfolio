88232c0c323f5050572b921ba84508e1c4241150
Himanshu Gunwant <69423776+monster-anshu@users.noreply.github.com>
2024-05-27 13:51:28 +0530

    change download cn button file name

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -167,7 +167,7 @@ function Landing() {
                             {headerData.resumePdf && (
                                 <a
                                     href={headerData.resumePdf}
-                                    download="resume"
+                                    download="Himanshu Gunwant's Resume"
                                     target="_blank"
                                     rel="noreferrer"
                                 >
